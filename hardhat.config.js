9155f094628ba80dc246fec83b62e318eab72c51
Tickether <tickether@gmail.com>
2022-10-07 02:09:15 +0000

    gas fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -17,15 +17,13 @@ module.exports = {
   networks: {
     mainnet: {
       url: process.env.REACT_APP_MAINNET_RPC_URL,
-      accounts: [process.env.REACT_APP_PROD_PRIVATE_KEY]
-    },
-    rinkeby: {
-      url: process.env.REACT_APP_RINKBEY_RPC_URL,
-      accounts: [process.env.REACT_APP_PRIVATE_KEY]
+      accounts: [process.env.REACT_APP_PROD_PRIVATE_KEY],
+      blockGasLimit: 2400000
     },
     goerli: {
       url: process.env.REACT_APP_GOERLI_RPC_URL,
-      accounts: [process.env.REACT_APP_PRIVATE_KEY]
+      accounts: [process.env.REACT_APP_PRIVATE_KEY],
+      blockGasLimit: 2400000
     },
   },
   etherscan: {
